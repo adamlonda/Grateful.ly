0721b215cd0d744fab7d27bee7dedceada4200f7
Adam Londa <adamlonda@pm.me>
2021-09-11 12:29:59 +0200

    Xcode project creation

---
+++ Grateful.ly/Grateful.ly/Grateful_lyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grateful_lyApp.swift
+//  Grateful.ly
+//
+//  Created by Adam Londa on 11.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct Grateful_lyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
